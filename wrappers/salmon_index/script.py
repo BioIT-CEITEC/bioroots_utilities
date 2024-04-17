67feb85bb250a01780952756b83a353916efb123
Alejandro Medaglia <amedagliamata2@gmail.com>
2024-04-17 11:41:49 +0000

    corrected salmon index generaton to exclude mashmap script

--- wrappers/salmon_index/script.py
+++ wrappers/salmon_index/script.py
@@ -19,15 +19,38 @@ f = open(snakemake.log.run, 'wt')
 f.write("## CONDA:\n"+version+"\n")
 f.close()
 
-command = "$(which time) --verbose sh " + snakemake.params.script + " -j 2 -a " + snakemake.input.gtf + \
-          " -g " + snakemake.input.gen + " -t " + snakemake.input.cds + " -o " + dirname(snakemake.output.gen) + " >> " + snakemake.log.run
-f = open(snakemake.log.run, 'wt')
-f.write("## CONDA:\n"+version+"\n")
+#command = "$(which time) sh " + snakemake.params.script + " -j 2 -a " + snakemake.input.gtf + \
+#          " -g " + snakemake.input.gen + " -t " + snakemake.input.cds + " -o " + dirname(snakemake.output.gen) + " >> " + snakemake.log.run
+#f = open(snakemake.log.run, 'at')
+#f.write("## CONDA:\n"+command+"\n")
+#f.close()
+#shell(command)
+command = "mkdir -p " + dirname(snakemake.output.dec)
+f = open(snakemake.log.run, "at")
+f.write("COMMAND:\n"+command+"\n")
 f.close()
 shell(command)
 
-command = "$(which time) --verbose salmon index -t " + snakemake.output.gen + " -d " + snakemake.output.dec + " -i " + snakemake.params.folder + " >> " + snakemake.log.run
-f = open(snakemake.log.run, 'wt')
-f.write("## CONDA:\n"+version+"\n")
+command = "grep '^>' " + snakemake.input.gen + " | cut -d ' ' -f 1 > "+snakemake.output.dec #+ " >> " + snakemake.log.run + "  2>&1 "
+f = open(snakemake.log.run, "at")
+f.write("COMMAND:\n"+command+"\n")
+f.close()
+shell(command)
+
+command = "sed -e 's/>//g' "+snakemake.output.dec+" > decoy.tmp ; mv decoy.tmp "+snakemake.output.dec#+"  >> " + snakemake.log.run + "  2>&1 "
+f = open(snakemake.log.run,"at")
+f.write("COMMAND:\n"+command+"\n")
+f.close()
+shell(command)
+
+command = "cat " + snakemake.input.cdna + " " + snakemake.input.gen + " > "+snakemake.output.gen#+" >> " + snakemake.log.run+ "  2>&1 "
+f = open(snakemake.log.run, 'at')
+f.write("COMMAND:\n"+command+'\n')
+f.close()
+shell(command)
+
+command = "$(which time) salmon index -t " + snakemake.output.gen + " -d " + snakemake.output.dec + " -p 12 -i " + snakemake.params.folder + " --gencode " #+ snakemake.log.run+ "  2>&1 "
+f = open(snakemake.log.run, 'at')
+f.write("## CONDA:\n"+command+"\n")
 f.close()
 shell(command)
