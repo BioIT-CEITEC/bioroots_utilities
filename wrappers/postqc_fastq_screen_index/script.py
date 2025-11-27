00c48bad564a08db6e574b2aceac4740afa2f779
Nicolas Blavet <46704751+blavetn@users.noreply.github.com>
2025-11-27 08:30:06 +0100

    Refactor command to extract rRNA data

--- wrappers/postqc_fastq_screen_index/script.py
+++ wrappers/postqc_fastq_screen_index/script.py
@@ -28,7 +28,7 @@ f.write("## COMMAND: " + command + "\n")
 shell(command)
 
 # extract rRNA data and build BOWTIE2 index
-command = "cat "+ snakemake.input.ncbi_annot + " | grep 'gbkey=rRNA' | grep -v 'ribosomal RNA protein' > " + snakemake.params.rRNA_prefix + ".gff 2>> " + snakemake.log.run + " || echo '## INFO: Command returned non-zero status. Probably, there are no gbkey=rRNA lines.' >> " + snakemake.log.run + " 2>&1"
+command = "grep 'gbkey=rRNA'" + snakemake.input.ncbi_annot + " | grep -v 'ribosomal RNA protein' > " + snakemake.params.rRNA_prefix + ".gff 2>> " + snakemake.log.run + " || echo '## INFO: Command returned non-zero status. Probably, there are no gbkey=rRNA lines.' >> " + snakemake.log.run + " 2>&1"
 f.write("## COMMAND: "+command+"\n")
 shell(command)
 
