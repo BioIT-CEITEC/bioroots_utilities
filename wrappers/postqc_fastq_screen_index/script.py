ab5a93947f4829789ca29ae038bccd4ab573beeb
Nicolas Blavet <46704751+blavetn@users.noreply.github.com>
2025-11-27 14:10:04 +0100

    Fix string conversion for rRNA_prefix in command

--- wrappers/postqc_fastq_screen_index/script.py
+++ wrappers/postqc_fastq_screen_index/script.py
@@ -65,7 +65,7 @@ else:
   f.close()
   shell(command)
 
-  command = BOWTIE2_BUILD + " --threads " + str(snakemake.threads) + " " + rRNA_prefix + " " + snakemake.params.bowtie2_indexes_fasta + os.path.basename(snakemake.params.rRNA_prefix) + " >> " + LOG_RUN
+  command = BOWTIE2_BUILD + " --threads " + str(snakemake.threads) + " " + str(snakemake.params.rRNA_prefix) + " " + snakemake.params.bowtie2_indexes_fasta + os.path.basename(snakemake.params.rRNA_prefix) + " >> " + LOG_RUN
   f = open(LOG_RUN, 'a+')
   f.write("## COMMAND:\n"+command+"\n")
   f.close()
